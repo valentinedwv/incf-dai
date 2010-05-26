8dc624b53e9b6bcdc3a66d7961e7184049c9db00
davlit0917 <davlit0917@7cbae6f4-d32f-11de-a711-9fae7a67fc1c>
2010-05-26 23:16:39 +0000

    Use WPS spec for XML.

--- atlas-server/trunk/src/main/java/org/incf/atlas/server/central/resource/CapabilitiesResource.java
+++ atlas-server/trunk/src/main/java/org/incf/atlas/server/central/resource/CapabilitiesResource.java
@@ -5,6 +5,7 @@ package org.incf.atlas.server.central.resource;
 //import generated.ServiceIdentification;
 //import generated.ServiceProvider;
 
+import java.rmi.server.Operation;
 import java.util.Map;
 import java.util.Set;
 
@@ -15,15 +16,19 @@ import javax.xml.bind.Marshaller;
 import javax.xml.namespace.QName;
 
 import net.opengis.ows._1.CodeType;
+import net.opengis.ows._1.ContactType;
 import net.opengis.ows._1.LanguageStringType;
+import net.opengis.ows._1.OnlineResourceType;
+import net.opengis.ows._1.OperationsMetadata;
+import net.opengis.ows._1.ResponsiblePartySubsetType;
 import net.opengis.ows._1.ServiceIdentification;
+import net.opengis.ows._1.ServiceProvider;
 import net.opengis.wps._1_0.Languages;
 import net.opengis.wps._1_0.LanguagesType;
 import net.opengis.wps._1_0.ObjectFactory;
 import net.opengis.wps._1_0.ProcessBriefType;
 import net.opengis.wps._1_0.ProcessOfferings;
 import net.opengis.wps._1_0.WPSCapabilitiesType;
-import net.opengis.wps._1_0.WSDL;
 
 import org.incf.atlas.server.central.util.AtlasNamespacePrefixMapper;
 import org.restlet.Context;
@@ -48,24 +53,20 @@ public class CapabilitiesResource extends Resource {
 	 * /incf-services/service/ABAServiceController?request=GetCapabilities&output=xml
 	 */
 	
-	private String req;
-	private String output;
+	private String responseFormat;
 
 	public CapabilitiesResource(Context context, Request request, Response response) {
 		super(context, request, response);
 		
-		String query = (String) getRequest().getAttributes().get("query");
-		Form queryString = new Form(query);
-//		Form queryString = request.getEntityAsForm();
-		req = queryString.getValues("request");
-		output = queryString.getValues("output");
+		responseFormat = (String) 
+		        getRequest().getAttributes().get("ResponseFormat");
 		
-		logger.debug("*****query: {}", query);
-		Set<String> names = queryString.getNames();
-		Map<String, String> map = queryString.getValuesMap();
-		for (String name : names) {
-			logger.debug("name: {}, value: {}", name, map.get(name));
-		}
+//		logger.debug("*****query: {}", query);
+//		Set<String> names = queryString.getNames();
+//		Map<String, String> map = queryString.getValuesMap();
+//		for (String name : names) {
+//			logger.debug("name: {}, value: {}", name, map.get(name));
+//		}
 		
 		getVariants().add(new Variant(MediaType.APPLICATION_XML));
 		
@@ -85,151 +86,96 @@ public class CapabilitiesResource extends Resource {
 		ObjectFactory of = new ObjectFactory();
 		net.opengis.ows._1.ObjectFactory owsFac = new net.opengis.ows._1.ObjectFactory();
 		
-		WPSCapabilitiesType capeType = of.createWPSCapabilitiesType();
+		WPSCapabilitiesType capabilities = of.createWPSCapabilitiesType();
 		
 		// attributes
-	    capeType.setVersion("0.0");
-	    capeType.setUpdateSequence("some update sequence");
-		capeType.setService("test service");
-		capeType.setLang("EN");
+	    capabilities.setVersion("0.0");
+	    capabilities.setUpdateSequence("some update sequence");
+		capabilities.setService("test service");
+		capabilities.setLang("EN");
 
+		// service identification
 	    ServiceIdentification si = owsFac.createServiceIdentification();
-	    LanguageStringType lstTitle = owsFac.createLanguageStringType();
-	    lstTitle.setValue("ABA Services");
-	    si.getTitle().add(lstTitle);
-	    LanguageStringType lstAbs = owsFac.createLanguageStringType();
-	    lstAbs.setValue("ABA Services are created to access ....");
-	    si.getAbstract().add(lstAbs);
-	    CodeType ct = owsFac.createCodeType();
-	    ct.setValue("WPS");
-	    si.setServiceType(ct);
+	    LanguageStringType siTitle = owsFac.createLanguageStringType();
+	    siTitle.setValue("ABA Services");
+	    si.getTitle().add(siTitle);
+	    LanguageStringType siAbstract = owsFac.createLanguageStringType();
+	    siAbstract.setValue("ABA Services are created to access ....");
+	    si.getAbstract().add(siAbstract);
+	    CodeType serviceType = owsFac.createCodeType();
+	    serviceType.setValue("WPS");
+	    si.setServiceType(serviceType);
 	    si.getServiceTypeVersion().add("0.2.41.0.0");
 	    si.setFees("NONE");
 	    si.getAccessConstraints().add("NONE");
-	    capeType.setServiceIdentification(si);
-//	    
-//	    ServiceProvider sp = owsFac.createServiceProvider();
-//	    capeType.setServiceProvider(sp);
-//	    
-//	    OperationsMetadata om = owsFac.createOperationsMetadata();
-//	    capeType.setOperationsMetadata(om);
-		
-		
-
+	    capabilities.setServiceIdentification(si);
 	    
-	    WSDL wsdl0 = of.createWSDL();
-		wsdl0.setHref("http://abc.dev.ghi/jkl/href0");
-		capeType.setWSDL(wsdl0);
+	    // service provider
+        ServiceProvider serviceProvider = owsFac.createServiceProvider();
+        serviceProvider.setProviderName("Asif Memon");
+        capabilities.setServiceProvider(serviceProvider);
+        
+        // operations metadata
+        OperationsMetadata operationsMetadata = 
+                owsFac.createOperationsMetadata();
+        net.opengis.ows._1.Operation operation = owsFac.createOperation();
+        operation.setName("GetCapabilities");
+        operationsMetadata.getOperation().add(operation);
+        capabilities.setOperationsMetadata(operationsMetadata);
+
+	    // process offerings
+        ProcessOfferings processOfferings = of.createProcessOfferings();
+
+        // process 1
+        ProcessBriefType process1 = of.createProcessBriefType();
+        CodeType identifier1 = owsFac.createCodeType();
+        identifier1.setValue("GetCorrelationMapByPOI");
+        process1.setIdentifier(identifier1);
+        LanguageStringType title1 = owsFac.createLanguageStringType();
+        title1.setValue("Get Correlation Map");
+        process1.setTitle(title1);
+        LanguageStringType abstract1 = owsFac.createLanguageStringType();
+        abstract1.setValue("This method will return the URL and load the "
+                + "correlation map interface in the browser, for a point of "
+                + "interest (POI) in a specified Spatial Reference System " 
+                + "(SRS)");
+        process1.setAbstract(abstract1);
+        processOfferings.getProcess().add(process1);
+        
+        // process 2
+        ProcessBriefType process2 = of.createProcessBriefType();
+        CodeType identifier2 = owsFac.createCodeType();
+        identifier2.setValue("GetStructureNamesByPOI");
+        process2.setIdentifier(identifier2);
+        LanguageStringType title2 = owsFac.createLanguageStringType();
+        title2.setValue("Get Structure Names by POI");
+        process2.setTitle(title2);
+        LanguageStringType abstract2 = owsFac.createLanguageStringType();
+        abstract2.setValue("Returns Fine/Anatomic structures segmented for the "
+                + "POI in a specified SRS");
+        process2.setAbstract(abstract2);
+		processOfferings.getProcess().add(process2);
 		
-		// block 1
-		ProcessBriefType pbt1 = of.createProcessBriefType();
-		pbt1.getProfile().add("http://abc.dev.ghi/jkl/profile-1a");
-		pbt1.getProfile().add("http://abc.dev.ghi/jkl/profile-1b");
-		pbt1.setProcessVersion("v1");
-		WSDL wsdl1 = of.createWSDL();
-		wsdl1.setHref("http://abc.dev.ghi/jkl/href1");
-		pbt1.setWSDL(wsdl1);
+		capabilities.setProcessOfferings(processOfferings);
 		
-		ProcessBriefType pbt2 = of.createProcessBriefType();
-		pbt2.getProfile().add("http://abc.dev.ghi/jkl/profile-2a");
-		pbt2.getProfile().add("http://abc.dev.ghi/jkl/profile-2b");
-		pbt2.setProcessVersion("v2");
-		WSDL wsdl2 = of.createWSDL();
-		wsdl2.setHref("http://abc.dev.ghi/jkl/href2");
-		pbt2.setWSDL(wsdl2);
-		
-		ProcessOfferings po = of.createProcessOfferings();
-		po.getProcess().add(pbt1);
-		po.getProcess().add(pbt2);
-		
-		capeType.setProcessOfferings(po);
-		
-		Languages langs = of.createLanguages();
-		Languages.Default ld = of.createLanguagesDefault();
-		ld.setLanguage("EN");
-		LanguagesType lt = of.createLanguagesType();
-		lt.getLanguage().add("EN");
-		lt.getLanguage().add("GE");
-		langs.setDefault(ld);
-		langs.setSupported(lt);
-		capeType.setLanguages(langs);
-		
-		// block 2
-//		ServiceIdentification serviceIdentification = 
-//			of.createServiceIdentification();
-//		serviceIdentification.setTitle("ABA Services");
-//		serviceIdentification.setAbstract("ABA Services are created to access "
-//				+ "various aba atlas space features that are offered by UCSD "
-//				+ "to its clients.");
-//		serviceIdentification.setServiceType("WPS");
-//		serviceIdentification.setServiceVersion("0.2.4");
-//		serviceIdentification.setFees("NONE");
-//		serviceIdentification.setAccessConstraints("NONE");
-//		
-//		ServiceProvider serviceProvider = of.createServiceProvider();
-//		serviceProvider.setProviderName("Asif Memon");
-//		serviceProvider.setServiceContact("amemon@ncmir.ucsd.edu");
-//		
-//		Capabilities capabilities = of.createCapabilities();
-//		capabilities.setServiceIdentification(serviceIdentification);
-//		capabilities.setServiceProvider(serviceProvider);
+		// languages
+		Languages languages = of.createLanguages();
+		Languages.Default defaultLanguage = of.createLanguagesDefault();
+		defaultLanguage.setLanguage("en-US");
+		LanguagesType supportedLanguages = of.createLanguagesType();
+		supportedLanguages.getLanguage().add("en-US");
+		languages.setDefault(defaultLanguage);
+		languages.setSupported(supportedLanguages);
+		capabilities.setLanguages(languages);
 		
 		// generate representation based on media type
 		if (variant.getMediaType().equals(MediaType.APPLICATION_XML)) {
-//			return new JaxbRepresentation<Capabilities>(capabilities);
-//			return new JaxbRepresentation<WPSCapabilitiesType>(capeType);
-//			JAXBElement<WPSCapabilitiesType> je = of.createCapabilities(capeType);
-			return getDomRepresentation(capeType);
-//			return getDomRepresentation(je);
+			return getDomRepresentation(capabilities);
 		}
 		
 		return null;
 	}
 	
-	// --------------------------------
-	
-	/**
-	 * For the server. Get a Restlet DomRepresentation of the given object,
-	 * which should be a generated class type.
-	 * 
-	 * @param object
-	 *            - generated class type.
-	 * @return - the DomRepresentation that is constructed from that generated
-	 *         class type
-	 * @see DomRepresentation
-	 */
-//	@SuppressWarnings("unchecked")
-//	public static synchronized DomRepresentation getDomRepresentation(
-//			JAXBElement<WPSCapabilitiesType> je) {
-//
-//		// Class typeClass = object.getClass();
-//		// return getDomRepresentation(object, typeClass);
-//		DomRepresentation representation = null;
-//		try {
-//			// create representation and get its empty dom
-//			representation = new DomRepresentation(MediaType.TEXT_XML);
-//			Document d = representation.getDocument();
-//
-//			final Marshaller marshaller = getWBCJAXBContext()
-//					.createMarshaller();
-//
-//			// marshal object into representation's dom
-////			Class clazz = object.getClass();
-////			QName qName = new QName(
-//////					"http://wholebrainproject.org/wbc/generated", 
-////					"http://www.opengis.net/wps/1.0.0",
-////					clazz.getSimpleName());
-////			JAXBElement jaxbElement = new JAXBElement(qName, clazz, object);
-////			marshaller.marshal(jaxbElement, d);
-//			marshaller.marshal(je, d);
-//
-//		} catch (Exception e) {
-//			throw new RuntimeException(e);
-//		}
-//
-//		return representation;
-//	}
-
 	/**
 	 * For the server. Get a Restlet DomRepresentation of the given object,
 	 * which should be a generated class type.
@@ -243,9 +189,6 @@ public class CapabilitiesResource extends Resource {
 	@SuppressWarnings("unchecked")
 	public static synchronized DomRepresentation getDomRepresentation(
 			Object object) {
-
-		// Class typeClass = object.getClass();
-		// return getDomRepresentation(object, typeClass);
 		DomRepresentation representation = null;
 		try {
 			// create representation and get its empty dom
@@ -261,23 +204,13 @@ public class CapabilitiesResource extends Resource {
 			// marshal object into representation's dom
 			Class clazz = object.getClass();
 			QName qName = new QName(
-//					"http://wholebrainproject.org/wbc/generated", 
 					"http://www.opengis.net/wps/1.0.0",
 					"Capabilities");
-					//clazz.getSimpleName());
 			JAXBElement jaxbElement = new JAXBElement(qName, clazz, object);
 			marshaller.marshal(jaxbElement, d);
-
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
-
-		
-		
-		representation.putNamespace("ows", "http://www.opengis.net/ows/1.1");
-		representation.putNamespace("wps", "http://www.opengis.net/wps/1.0.0");
-		
-		
 		return representation;
 	}
 
@@ -292,9 +225,7 @@ public class CapabilitiesResource extends Resource {
 	public static JAXBContext getWBCJAXBContext() {
 		if (jaxbContext == null) {
 			try {
-				jaxbContext = JAXBContext.newInstance(
-//				"org.wholebrainproject.wbc.generated");
-				"net.opengis.wps._1_0");
+				jaxbContext = JAXBContext.newInstance("net.opengis.wps._1_0");
 			} catch (JAXBException e) {
 				throw new RuntimeException(e);
 			}

a25b4cdaac328a90b4d3b127999ceebff20d46b6
davlit0917 <davlit0917@7cbae6f4-d32f-11de-a711-9fae7a67fc1c>
2010-06-10 18:54:22 +0000

    Adding classes to support GetCapabilities, ping, favicon plus routing mods to server. Adding XML validator for checking responses.

---
+++ atlas-aba/trunk/src/main/java/org/incf/atlas/aba/resource/FaviconResource.java
@@ -0,0 +1,62 @@
+package org.incf.atlas.aba.resource;
+
+import java.io.File;
+import java.net.URISyntaxException;
+
+import org.restlet.Context;
+import org.restlet.data.MediaType;
+import org.restlet.data.Request;
+import org.restlet.data.Response;
+import org.restlet.resource.FileRepresentation;
+import org.restlet.resource.Representation;
+import org.restlet.resource.Resource;
+import org.restlet.resource.ResourceException;
+import org.restlet.resource.Variant;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FaviconResource extends Resource {
+	
+	private final Logger logger = LoggerFactory.getLogger(
+			FaviconResource.class);
+
+	// resource object
+	private File favicon;
+	
+	public FaviconResource(Context context, Request request, 
+			Response response) {
+		super(context, request, response);
+		
+		// get resource
+		try {
+			favicon = new File(this.getClass().getResource("/favicon.ico")
+					.toURI());
+		} catch (URISyntaxException e) {
+			logger.error("Error getting favicon.", e);
+		}
+		
+		// if found, set media type
+		// by default: available is true; modifiable is false
+		if (favicon != null) {
+			getVariants().add(new Variant(MediaType.IMAGE_ICON));
+		} else {
+			setAvailable(false);
+		}
+	}
+
+	/**
+	 * Handle GET requests. This method responds to root ("/") GET requests,
+	 * which are typically requests for a favicon.
+	 */
+	@Override
+	public Representation represent(Variant variant) throws ResourceException {
+		
+		// generate representation based on media type
+		if (variant.getMediaType().equals(MediaType.IMAGE_ICON)) {
+			return new FileRepresentation(favicon, MediaType.IMAGE_ICON);
+		}
+		
+		return null;
+	}
+
+}
---
+++ atlas-aba/trunk/src/main/java/org/incf/atlas/aba/util/XmlValidator.java
@@ -0,0 +1,102 @@
+package org.incf.atlas.aba.util;
+
+import java.io.File;
+
+import javax.xml.XMLConstants;
+import javax.xml.transform.sax.SAXSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+
+/**
+ * Simple, all-JDK, SAX (not DOM) XML validator.
+ * 
+ * @author David Little
+ */
+public class XmlValidator {
+
+	private Schema schema;
+
+	public XmlValidator(String schemaFile) {
+		try {
+			String language = XMLConstants.W3C_XML_SCHEMA_NS_URI;
+			SchemaFactory factory = SchemaFactory.newInstance(language);
+			schema = factory.newSchema(new File(schemaFile));
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		}
+	}
+
+	public boolean validate(String xmlToValidate) {
+
+		// allows for continuing validation after errors (rather that simply
+		// stopping at that point
+		ValidatorErrorHandler errorHandler = new ValidatorErrorHandler();
+
+		try {
+			Validator validator = schema.newValidator();
+			validator.setErrorHandler(errorHandler);
+
+			// prepare XML file as SAX source
+			SAXSource source = new SAXSource(new InputSource(this.getClass()
+					.getResourceAsStream(xmlToValidate)));
+
+			// validate against the schema
+			validator.validate(source);
+
+		} catch (SAXParseException e) {
+			System.out.printf("Line: %d - %s%n", e.getLineNumber(), e
+					.toString());
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		}
+		if (errorHandler.exceptions()) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	private class ValidatorErrorHandler implements ErrorHandler {
+		
+		private boolean exceptions = false;
+
+		public void error(SAXParseException e) throws SAXException {
+			exceptions = true;
+			System.out.printf("Line: %d - %s%n", e.getLineNumber(), e.toString());
+		}
+
+		public void fatalError(SAXParseException e) throws SAXException {
+			exceptions = true;
+			throw e; 
+		}
+
+		public void warning(SAXParseException e) throws SAXException {
+			exceptions = true;
+			System.out.printf("Line: %d - %s%n", e.getLineNumber(), e.toString());
+		}
+		
+		public boolean exceptions() {
+			return exceptions;
+		}
+
+	}
+	
+	// dev testing only
+	public static void main(String[] a) {
+		final String SCHEMA = "target/classes/WBC_v0.4.xsd";
+		final String XML = "/wholeWBCState.xml";
+		XmlValidator validator = new XmlValidator(SCHEMA);
+		if (validator.validate(XML)) {
+			System.out.println("Passed");
+		} else {
+			System.out.println("Failed");
+		}
+	}
+
+}

e0704a761c9b9c2bd3e1c5c8034b42a00d94b99f
david.valentine <david.valentine@7cbae6f4-d32f-11de-a711-9fae7a67fc1c>
2010-07-28 01:39:31 +0000

    Update CoordinateTransformationChain to reflect the document. CorrelationMapResponse, RetrieveImage2DResponse,ImagesResponse,ImagesByUriResponse all working and validating

---
+++ waxml/trunk/AtlasXmlBeanExamples/src/org/incf/atlas/xmlbeans/examples/Utilities.java
@@ -0,0 +1,18 @@
+package org.incf.atlas.xmlbeans.examples;
+
+import java.util.HashMap;
+
+public class Utilities {
+	public static HashMap SuggestedNamespaces() {
+		HashMap suggestedPrefixes = new HashMap();
+		
+		suggestedPrefixes.put("http://www.opengis.net/gml/3.2", "gml");
+		suggestedPrefixes.put("http://www.incf.org/WaxML/", "wax");
+		suggestedPrefixes.put("http://www.w3.org/2001/XMLSchema-instance", "xsi");
+
+		
+		return suggestedPrefixes;
+	}
+	
+	
+}

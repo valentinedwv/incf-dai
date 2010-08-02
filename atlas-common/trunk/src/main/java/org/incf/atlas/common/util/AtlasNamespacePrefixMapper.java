8004a7cd262e0b48b404b606664b2171c8031c74
davlit0917 <davlit0917@7cbae6f4-d32f-11de-a711-9fae7a67fc1c>
2010-08-02 06:38:21 +0000

    Added some Java classes to common.

---
+++ atlas-common/trunk/src/main/java/org/incf/atlas/common/util/AtlasNamespacePrefixMapper.java
@@ -0,0 +1,33 @@
+package org.incf.atlas.common.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.sun.xml.bind.marshaller.NamespacePrefixMapper;
+
+public class AtlasNamespacePrefixMapper extends NamespacePrefixMapper {
+	
+	private static final String[][] MAP = {
+		{ "http://www.opengis.net/ows/1.1", "ows" },
+		{ "http://www.opengis.net/wps/1.0.0", "wps"},
+		{ "http://www.w3.org/1999/xlink", "xlink" },
+		{ "http://www.opengis.net/gml/3.2", "gml" },
+		{ "http://www.incf.org/waxML", "wax" },
+	};
+	
+	private Map<String, String> prefixMap;
+	
+	public AtlasNamespacePrefixMapper() {
+		prefixMap = new HashMap<String, String>();
+		for (int i = 0; i < MAP.length; i++) {
+			prefixMap.put(MAP[i][0], MAP[i][1]);
+		}
+	}
+
+	@Override
+	public String getPreferredPrefix(String namespaceUri, String suggestion, 
+			boolean requirePrefix) {
+		return prefixMap.get(namespaceUri);
+	}
+
+}

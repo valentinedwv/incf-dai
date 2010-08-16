c51c190457de21702dbaf7519e805575548d4432
davlit0917 <davlit0917@7cbae6f4-d32f-11de-a711-9fae7a67fc1c>
2010-08-16 07:35:19 +0000

    Initial commit.

---
+++ atlas-whs/trunk/src/main/java/org/incf/atlas/whs/resource/PingResource.java
@@ -0,0 +1,62 @@
+package org.incf.atlas.whs.resource;
+
+import org.restlet.Context;
+import org.restlet.data.MediaType;
+import org.restlet.data.Request;
+import org.restlet.data.Response;
+import org.restlet.resource.Representation;
+import org.restlet.resource.Resource;
+import org.restlet.resource.ResourceException;
+import org.restlet.resource.StringRepresentation;
+import org.restlet.resource.Variant;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PingResource extends Resource {
+
+	private final Logger logger = LoggerFactory.getLogger(getClass());
+	
+	// resource object
+	private String pingResponse;
+
+	public PingResource(Context context, Request request, Response response) {
+		super(context, request, response);
+		
+		logger.debug("Instantiated {}.", getClass());
+		
+		String pingType = (String) request.getAttributes().get("pingType"); 
+//		if (pingType.equals("server")) {
+			StringBuilder buf = new StringBuilder();
+			buf.append("Server responds to ping type: ").append(pingType);
+			pingResponse = buf.toString();
+//		}
+
+		// if found, set media type
+		// by default: available is true; modifiable is false
+		if (pingResponse != null) {
+			getVariants().add(new Variant(MediaType.TEXT_PLAIN));
+		} else {
+			setAvailable(false);
+		}
+		
+		logger.debug("pingType: {}", pingType);
+	}
+
+	/* 
+	 * Handle GET requests.
+	 * 
+	 * (non-Javadoc)
+	 * @see org.wholebrainproject.wbc.server.resource.DataRepositoryResource#represent(org.restlet.resource.Variant)
+	 */
+	@Override
+	public Representation represent(Variant variant) throws ResourceException {
+		
+		// generate representation based on media type
+		if (variant.getMediaType().equals(MediaType.TEXT_PLAIN)) {
+			return new StringRepresentation(pingResponse);
+		}
+		
+		return null;
+	}
+
+}
---
+++ atlas-whs/trunk/src/main/java/org/incf/atlas/whs/resource/ProcessDescriptions.java
@@ -0,0 +1,69 @@
+package org.incf.atlas.whs.resource;
+
+import java.io.File;
+
+import org.incf.atlas.common.util.ExceptionCode;
+import org.incf.atlas.common.util.ExceptionHandler;
+import org.restlet.Context;
+import org.restlet.data.MediaType;
+import org.restlet.data.Request;
+import org.restlet.data.Response;
+import org.restlet.resource.FileRepresentation;
+import org.restlet.resource.Representation;
+import org.restlet.resource.ResourceException;
+import org.restlet.resource.Variant;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+// test:
+// http://incf-dev-local.crbs.ucsd.edu:8080/atlas-whs?service=WPS&version=1.0.0&request=DescribeProcess
+
+public class ProcessDescriptions extends BaseResouce {
+
+	private final Logger logger = LoggerFactory.getLogger(getClass());
+	
+    // cached file
+    private static final String RESPONSE_FILE_NAME = "ProcessDescriptions.xml";
+    
+	public ProcessDescriptions(Context context, Request request,
+			Response response) {
+		super(context, request, response);
+
+        logger.debug("Instantiated {}.", getClass());
+	}
+
+	/* 
+	 * Handle GET requests.
+	 * 
+	 * (non-Javadoc)
+	 * @see org.wholebrainproject.wbc.server.resource.DataRepositoryResource#represent(org.restlet.resource.Variant)
+	 */
+	@Override
+	public Representation represent(Variant variant) throws ResourceException {
+		
+        // if there are exceptions, send an excepton report
+        if (exceptionHandler != null) {
+            logger.error("Exception Report returned to client: \n{}", 
+                    exceptionHandler.toString());
+            return getExceptionRepresentation();
+        }
+        
+        // look for cached file fisrt
+        File cachedResponse = new File(cacheDir, RESPONSE_FILE_NAME);
+        if (cachedResponse.exists()) {
+            return new FileRepresentation(cachedResponse, 
+                    MediaType.APPLICATION_XML);
+        }
+    
+        // prepare an ExceptionReport
+        String message = "File " + RESPONSE_FILE_NAME + " not found.";
+        ExceptionHandler exHandler = getExceptionHandler();
+        exHandler.addExceptionToReport(ExceptionCode.NOT_APPLICABLE_CODE, null, 
+                new String[] { message });
+        logger.error(message);
+        
+        // generate xml
+        return exHandler.getDomExceptionReport();
+	}
+	
+}

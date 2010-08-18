36496f31ff0e489b08ad3986082e73acf3c8c887
davlit0917 <davlit0917@7cbae6f4-d32f-11de-a711-9fae7a67fc1c>
2010-08-18 19:29:21 +0000

    Change to XMLBeans exception handler.

--- atlas-aba/trunk/src/main/java/org/incf/atlas/aba/resource/Capabilities.java
+++ atlas-aba/trunk/src/main/java/org/incf/atlas/aba/resource/Capabilities.java
@@ -49,14 +49,15 @@ public class Capabilities extends BaseResouce {
 	@Override
 	public Representation represent(Variant variant) throws ResourceException {
 	    
-	    // if there are exceptions, send an excepton report
+	    // if there are exceptions, send an exception report
 	    if (exceptionHandler != null) {
+	    	String xmlReport = exceptionHandler.getXMLExceptionReport();
 	    	logger.error("Exception Report returned to client: \n{}", 
-	    			exceptionHandler.toString());
+	    			xmlReport);
 	        return getExceptionRepresentation();
 	    }
-		
-	    // look for cached file fisrt
+	    
+	    // look for cached file first
 	    File cachedResponse = new File(cacheDir, RESPONSE_FILE_NAME);
 	    if (cachedResponse.exists()) {
 	        return new FileRepresentation(cachedResponse, 
@@ -69,9 +70,7 @@ public class Capabilities extends BaseResouce {
         exHandler.addExceptionToReport(ExceptionCode.NOT_APPLICABLE_CODE, null, 
                 new String[] { message });
         logger.error(message);
-        
-        // generate xml
-        return exHandler.getDomExceptionReport();
+        return getExceptionRepresentation();
 	}
 	
 }

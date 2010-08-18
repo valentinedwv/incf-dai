6185e63a72b443cffb73368834184e11127f03ef
davlit0917 <davlit0917@7cbae6f4-d32f-11de-a711-9fae7a67fc1c>
2010-08-18 17:53:53 +0000

    Change to XMLBeans exception handler.

--- atlas-common/trunk/src/main/java/org/incf/atlas/common/util/ExceptionHandler.java
+++ atlas-common/trunk/src/main/java/org/incf/atlas/common/util/ExceptionHandler.java
@@ -1,17 +1,13 @@
 package org.incf.atlas.common.util;
 
-import java.io.StringWriter;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
+import org.apache.xmlbeans.XmlOptions;
 
-import net.opengis.ows._1.ExceptionReport;
-import net.opengis.ows._1.ExceptionType;
-import net.opengis.ows._1.ObjectFactory;
-
-import org.restlet.resource.Representation;
+import net.opengis.ows.x20.ExceptionReportDocument;
+import net.opengis.ows.x20.ExceptionType;
+import net.opengis.ows.x20.ExceptionReportDocument.ExceptionReport;
 
 /**
  * The constructor creates a WPS ExceptionReport object. This object has a list
@@ -22,10 +18,7 @@ import org.restlet.resource.Representation;
  */
 public class ExceptionHandler {
 	
-	public static final String NAMESPACE_URI = "http://www.opengis.net/ows/1.1";
-	public static final String CONTEXT_PATH = "net.opengis.ows._1";
-	
-	private ObjectFactory of;
+	private ExceptionReportDocument exceptionReportDoc;
 	private ExceptionReport exceptionReport;
 	
 	/**
@@ -35,10 +28,15 @@ public class ExceptionHandler {
 	 * @param language the ExceptionReport language
 	 */
 	public ExceptionHandler(String version, String language) {
-		of = new ObjectFactory();
-		exceptionReport = of.createExceptionReport();
+		exceptionReportDoc = ExceptionReportDocument.Factory.newInstance();
+		exceptionReport = exceptionReportDoc.addNewExceptionReport();
+
 		exceptionReport.setVersion(version);
-		exceptionReport.setLang(language);
+		
+		// lang attribute is optional
+		if (language != null) {
+			exceptionReport.setLang(language);
+		}
 	}
 	
 	/**
@@ -54,66 +52,68 @@ public class ExceptionHandler {
 			String locator, String[] exceptionText) {
 				
 		// handle one new exception
-		ExceptionType exceptionType = of.createExceptionType();
-		exceptionType.setExceptionCode(exceptionCode.toString());
-		exceptionType.setLocator(locator);
-		for (int i = 0; i < exceptionText.length; i++) {
-			exceptionType.getExceptionText().add(exceptionText[i]);
+		ExceptionType exception = exceptionReport.addNewException();
+		exception.setExceptionCode(exceptionCode.toString());
+		
+		// locator attribute is optional
+		if (locator != null) {
+			exception.setLocator(locator);
 		}
 		
-		// add it to report's list of exceptions
-		exceptionReport.getException().add(exceptionType);
+		// ExceptionText element(s) is optional
+		if (exceptionText != null && exceptionText.length !=0) {
+			exception.setExceptionTextArray(exceptionText);
+		}
 	}
 	
-	public ExceptionReport getExceptionReport() {
-		return exceptionReport;
-	}
+//	public ExceptionReport getExceptionReport() {
+//		return exceptionReport;
+//	}
 	
 	/**
-	 * Marshal the ExceptionReport into an XML string.
+	 * Generate the ExceptionReport as an XML string.
 	 * 
 	 * @return the ExceptionReport as an XML string
-	 * @throws JAXBException if any marshalling problem
 	 */
-	public String getXMLExceptionReport() throws JAXBException {
-		JAXBContext jc = JAXBContext.newInstance("net.opengis.ows._1");
-		Marshaller marshaller = jc.createMarshaller();
-		marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-//		marshaller.setProperty("com.sun.xml.bind.namespacePrefixMapper", 
-//				new AtlasNamespacePrefixMapper());
-		StringWriter out = new StringWriter();
-		marshaller.marshal(exceptionReport, out);
-		return out.toString();
+	public String getXMLExceptionReport() {
+		Map<String, String> suggestedPrefixes = new HashMap<String, String>();
+		suggestedPrefixes.put("http://www.opengis.net/ows/2.0", "ows");
+		XmlOptions opts = new XmlOptions();
+		opts.setSavePrettyPrint();
+		opts.setSaveSuggestedPrefixes(suggestedPrefixes);
+		opts.setSaveNamespacesFirst();
+		opts.setSaveAggressiveNamespaces();
+		opts.setUseDefaultNamespace();
+		return exceptionReportDoc.xmlText(opts);
 	}
 	
-	public Representation getDomExceptionReport() {
-		return XMLUtilities.getDomRepresentation(exceptionReport, 
-				NAMESPACE_URI, CONTEXT_PATH);
-	}
+//	public Representation getDomExceptionReport() {
+//		return XMLUtilities.getDomRepresentation(exceptionReport, 
+//				NAMESPACE_URI, CONTEXT_PATH);
+//	}
 	
-	public String toString() {
-		StringBuilder buf = new StringBuilder();
-		List<ExceptionType> exceptions = exceptionReport.getException();
-		for (ExceptionType exception : exceptions) {
-			buf.append("  Exception code: ");
-			buf.append(exception.getExceptionCode()).append('\n');
-			List<String> lines = exception.getExceptionText();
-			for (String line : lines) {
-				buf.append("    ").append(line).append('\n');
-			}
-		}
-		return buf.toString();
-	}
+//	public String toString() {
+//		StringBuilder buf = new StringBuilder();
+//		List<ExceptionType> exceptions = exceptionReport.getException();
+//		for (ExceptionType exception : exceptions) {
+//			buf.append("  Exception code: ");
+//			buf.append(exception.getExceptionCode()).append('\n');
+//			List<String> lines = exception.getExceptionText();
+//			for (String line : lines) {
+//				buf.append("    ").append(line).append('\n');
+//			}
+//		}
+//		return buf.toString();
+//	}
 	
 	// testing only
-	public static void main(String[] args) throws JAXBException {
+	public static void main(String[] args) /*throws JAXBException*/ {
 		ExceptionHandler eh = new ExceptionHandler("1.0.0", "en-US");
 		eh.addExceptionToReport(ExceptionCode.NOT_APPLICABLE_CODE, null, 
 				new String[] { "Line 1a.", "Line 1b." });
 		eh.addExceptionToReport(ExceptionCode.NOT_APPLICABLE_CODE, null, 
 				new String[] { "Line 2a.", "Line 2b." });
 		System.out.println(eh.getXMLExceptionReport());
-		System.out.println(eh.toString());
 	}
 
 }

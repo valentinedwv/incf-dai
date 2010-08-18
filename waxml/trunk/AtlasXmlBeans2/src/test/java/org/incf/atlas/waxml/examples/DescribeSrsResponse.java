911f2fee352f11bcfc6fef64cf749654640ac323
david.valentine <david.valentine@7cbae6f4-d32f-11de-a711-9fae7a67fc1c>
2010-08-18 22:56:53 +0000

    Update POM to inclue ows/exceptionReport Changes to XML schema

--- waxml/trunk/AtlasXmlBeans2/src/test/java/org/incf/atlas/waxml/examples/DescribeSrsResponse.java
+++ waxml/trunk/AtlasXmlBeans2/src/test/java/org/incf/atlas/waxml/examples/DescribeSrsResponse.java
@@ -2,6 +2,7 @@ package org.incf.atlas.waxml.examples;
 
 import static org.junit.Assert.assertTrue;
 
+
 import java.util.ArrayList;
 
 import javax.xml.namespace.QName;
@@ -14,6 +15,7 @@ import org.apache.xmlbeans.XmlOptions;
 import org.apache.xmlbeans.impl.values.XmlObjectBase;
 
 import org.incf.atlas.waxml.generated.*;
+import org.incf.atlas.waxml.generated.DescribeSRSResponseType.Slices;
 import org.incf.atlas.waxml.generated.ListSRSResponseType.*;
 import org.incf.atlas.waxml.generated.QueryInfoType.*;
 import org.incf.atlas.waxml.generated.SRSType.*;
@@ -21,6 +23,9 @@ import org.isotc211.x2005.gmd.CIResponsiblePartyType;
 import org.incf.atlas.waxml.utilities.*;
 import org.junit.Ignore;
 import org.junit.Test;
+
+import org.incf.atlas.waxml.examples.ListSRSResponse;
+import org.incf.atlas.waxml.examples.ListSRSResponse.*;
 
 public class DescribeSrsResponse   {
 	@Ignore("not ready")
@@ -75,32 +80,41 @@ public class DescribeSrsResponse   {
 		DescribeSRSResponseDocument document =	DescribeSRSResponseDocument.Factory.newInstance(); 
 		
 		DescribeSRSResponseType rootDoc =	document.addNewDescribeSRSResponse();
-		
-		
-		QueryUrl qurl =rootDoc.addNewQueryInfo().addNewQueryUrl();
-		qurl.setName("ListSRSs");
-		
-		
-	SRSType srs = 	 rootDoc.addNewSRS();
-		SRSType.Name srsname = srs.addNewName();
-		srsname.setStringValue("Mouse_ABAreference_1.0");
-		srsname.setUrn("INCF:0101");
-		srsname.setSrsCode("INCF:0101");
-		srsname.setSrsBase("ABAreference");
-		srsname.setSrsVersion("1.0");
-		srsname.setSpecies("Mouse");
-		
-		Incfdescription desc = srs.addNewDescription();
-		
-		AuthorType author = srs.addNewAuthor();
-		
-		IncfCodeType orgin = srs.addNewOrigin();
-		
-		Area area = srs.addNewArea();
-		
-		NeurodimensionsType neurodimensions = srs.addNewNeurodimensions();
-		
-		IncfDocumentCitation cite = srs.addNewSource();
-		return document;
+			
+			rootDoc.newCursor().insertComment("Test Comment");
+			ListSRSResponse.QueryInfoSrs(rootDoc.addNewQueryInfo(), "URL");
+			SRSList srsList = rootDoc.addNewSRSList();
+			SRSType srs1 =  srsList.addNewSRS();
+			ListSRSResponse.SrsExample1(srs1);
+			
+			Orientations o = rootDoc.addNewOrientations();
+			OrientationType orientaiton1 = o.addNewOrientation();
+			//orientation(orientaiton1,code,name);
+			ListSRSResponse.orientation(orientaiton1,"Left","Left");
+			orientaiton1 = o.addNewOrientation();
+			ListSRSResponse.orientation(orientaiton1,"Right","Right");
+			orientaiton1 = o.addNewOrientation();
+			ListSRSResponse.orientation(orientaiton1,"Ventral","Ventral");
+			orientaiton1 = o.addNewOrientation();
+			ListSRSResponse.orientation(orientaiton1,"Dorsal","Dorsal");
+			orientaiton1 = o.addNewOrientation();
+			ListSRSResponse.orientation(orientaiton1,"Posterior","Posterior");
+			orientaiton1 = o.addNewOrientation();
+			ListSRSResponse.orientation(orientaiton1,"Anterior","Anterior");
+			
+			Slices s = rootDoc.addNewSlices();
+			s.addNewSlice();
+			
+			return document;
+		}
+	
+	public static void exampleSlice(SliceType slice, int identifier){
+		slice.setOrientation(SliceType.Orientation.HORIZONTAL);
+		slice.setXOrientation("positive dorsal");
+		slice.setYOrientation("positive coronal"); 
+				slice.setConstant(1);
+				slice.setCode("REference Number for Slice");
+				
 	}
+	
 }

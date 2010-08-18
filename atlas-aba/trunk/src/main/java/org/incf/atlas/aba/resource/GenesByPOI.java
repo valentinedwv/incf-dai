6345fd4238e62929f12b85a442c973c5c7aefe38
davlit0917 <davlit0917@7cbae6f4-d32f-11de-a711-9fae7a67fc1c>
2010-08-18 19:29:21 +0000

    Change to XMLBeans exception handler.

---
+++ atlas-aba/trunk/src/main/java/org/incf/atlas/aba/resource/GenesByPOI.java
@@ -0,0 +1,135 @@
+package org.incf.atlas.aba.resource;
+
+import java.io.File;
+
+import org.incf.atlas.common.util.ExceptionCode;
+import org.incf.atlas.common.util.ExceptionHandler;
+import org.restlet.Context;
+import org.restlet.data.MediaType;
+import org.restlet.data.Request;
+import org.restlet.data.Response;
+import org.restlet.resource.FileRepresentation;
+import org.restlet.resource.Representation;
+import org.restlet.resource.ResourceException;
+import org.restlet.resource.Variant;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/* Email fr Ilya Aug 13
+Dave (and Asif),
+ 
+We’ve discussed relying on GetGenesByPOI as the first filtering step in the 
+Get2DImagesByPOI implementation. For ABA, GetGenesByPOI means wrapping an AGEA 
+request such as  
+ 
+http://mouse.brain-map.org/agea/GeneFinder.xml?seedPoint=6600,4000,5600
+ 
+and generating WaxML. In the previous version, we just displayed a URL of the 
+page, as in
+ 
+http://whs0.pdc.kth.se:8080/incf-services/service/ABAServiceController?request=GetGenesByPOI&SRSCode=ABAVoxel&x=263&y=159&z=227&output=txt
+ 
+ 
+In the current version, we cannot populate the entire set of elements in the 
+schema, but at least <Level> in our schema would be the same as <energy> in 
+AGEA output. I am not sure how to map other things, they look specific to AGEA 
+(see http://mouse.brain-map.org/pdf/AGEA.pdf ). I don’t think “rank” 
+corresponds to anything in our schema (“rank” is a precomputed ratio of 
+expression energy in a set of voxels that are in the top 1/3 of correlation 
+range, to expression energy in a larger region, i.e. all voxels above a 
+threshold).
+ 
+Not sure how you guys want to divide up the work, but I thought that since Dave 
+is already working on Get2DImagesByPOI, he might do the GetGenesByPOI for the 
+ABA hub as well? Let me know,
+ 
+Thanks,
+-          ilya 
+*/
+/*
+ GET: http://[host:port]/[hub]$service=WPS&version=[version]&request=Execute
+ &Identifier=GetGenesByPOI&DataInputs=srsName=[srsName];x=[x];y=[y];z=[z]
+ 
+ 
+ */
+
+public class GenesByPOI extends BaseResouce {
+	
+	private final Logger logger = LoggerFactory.getLogger(getClass());
+	
+	// cached file
+	private static final String RESPONSE_FILE_NAME = "Capabilities.xml";
+	
+	public GenesByPOI(Context context, Request request, Response response) {
+		super(context, request, response);
+		
+		logger.debug("Instantiated {}.", getClass());
+	}
+
+	/* 
+	 * Handle GET requests.
+	 * 
+	 * (non-Javadoc)
+	 * @see org.wholebrainproject.wbc.server.resource.DataRepositoryResource#represent(org.restlet.resource.Variant)
+	 */
+	@Override
+	public Representation represent(Variant variant) throws ResourceException {
+	    
+	    // if there are exceptions, send an excepton report
+	    if (exceptionHandler != null) {
+	    	logger.error("Exception Report returned to client: \n{}", 
+	    			exceptionHandler.toString());
+	        return getExceptionRepresentation();
+	    }
+		
+	    /*
+	     validate srsName - s/b AGEA?
+	     validate x y z
+	     validate filter - what to do with it?
+	     prepare http GET to ABA
+	     get ABA response
+	     how many <agea-rank>'s to return? criteria for which?
+	     aba provides
+	    	ageapositionid
+	      	count
+	      	energy
+	      	exampleimageid
+	      	genesymbol
+	      	imageseriesid
+	      	rank
+	     WaxML GenesResponse.xsd returns
+	     	Gene
+		     	Symbol
+		     		codeSpace
+		     		gml:id
+		     		symbol
+		     	MarkerAccessionId
+		     	    separator
+		     	    Prefix
+		     	    Identifier
+		     	    FullIdentifier
+		     	Name
+		     	Organism
+	     	ExpressionLevel
+	     		GeneSymbol xlin:href
+	     		Stage
+	     		Level
+	     		ResourceUri
+	    how to map aba response to Atlas response?
+	    do the mapping using XMLBeans objects
+	    return Atlas GenesResponse 		
+	     */
+
+	    
+        // prepare an ExceptionReport
+	    String message = "File " + RESPONSE_FILE_NAME + " not found.";
+        ExceptionHandler exHandler = getExceptionHandler();
+        exHandler.addExceptionToReport(ExceptionCode.NOT_APPLICABLE_CODE, null, 
+                new String[] { message });
+        logger.error(message);
+        
+        // TODO
+        return null;
+	}
+	
+}
--- atlas-aba/trunk/src/main/java/org/incf/atlas/aba/resource/UnrecognizedUri.java
+++ atlas-aba/trunk/src/main/java/org/incf/atlas/aba/resource/UnrecognizedUri.java
@@ -1,66 +1,41 @@
 package org.incf.atlas.aba.resource;
 
-import javax.xml.bind.JAXBException;
-
-import org.incf.atlas.aba.util.Constants;
-import org.incf.atlas.aba.util.ExceptionCode;
-import org.incf.atlas.aba.util.ExceptionHandler;
+import org.incf.atlas.common.util.ExceptionCode;
+import org.incf.atlas.common.util.ExceptionHandler;
 import org.restlet.Context;
-import org.restlet.data.MediaType;
 import org.restlet.data.Request;
 import org.restlet.data.Response;
-import org.restlet.data.Status;
 import org.restlet.resource.Representation;
-import org.restlet.resource.Resource;
 import org.restlet.resource.ResourceException;
-import org.restlet.resource.StringRepresentation;
 import org.restlet.resource.Variant;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class UnrecognizedUri extends Resource {
+public class UnrecognizedUri extends BaseResouce {
 
 	private final Logger logger = LoggerFactory.getLogger(getClass());
 	
 	String message;
 
-	public UnrecognizedUri(Context context, Request request, Response response) {
+	public UnrecognizedUri(Context context, Request request, 
+			Response response) {
 		super(context, request, response);
 		
 		logger.debug("Instantiated {}.", getClass());
 		
-		String uriSuffix = (String) request.getAttributes().get("uriSuffix"); 
-		message = String.format("Unrecognized URI suffix: %s", uriSuffix);
-		logger.warn(message);
+		message = "Unrecognized URI.";
 		
-		getVariants().add(new Variant(MediaType.APPLICATION_XML));
+		logger.info(message);
 	}
 
 	@Override
 	public Representation represent(Variant variant) throws ResourceException {
-		
-		logger.debug("Executing represent().");
-		logger.debug("Message: {}", message);
-		
-		// prepare an ExceptionReport
-		ExceptionHandler eh = new ExceptionHandler(
-				Constants.getInstance().getDefaultVersion());
-		eh.addExceptionToReport(ExceptionCode.NOT_APPLICABLE_CODE, null, 
-				new String[] { message });
-		
-		// generate xml
-		String exceptionReportXml = null;
-		try {
-			exceptionReportXml = eh.getXMLExceptionReport();
-		} catch (JAXBException e) {
-			throw new ResourceException(e);
-		}
-		
-		logger.debug("exceptionReportXml: {}", exceptionReportXml);
-
-		// return it
-		getResponse().setStatus(Status.CLIENT_ERROR_FORBIDDEN);
-		return new StringRepresentation(exceptionReportXml);
+        
+        // prepare an ExceptionReport
+        ExceptionHandler exHandler = getExceptionHandler();
+        exHandler.addExceptionToReport(ExceptionCode.NOT_APPLICABLE_CODE, null, 
+                new String[] { message });
+        return getExceptionRepresentation();
 	}
 
 }

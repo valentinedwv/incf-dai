ae9ae6c9bcf3ee38409c06b45fcadb4a98b79ecb
davlit0917 <davlit0917@7cbae6f4-d32f-11de-a711-9fae7a67fc1c>
2010-09-11 22:41:45 +0000

    Comment out import line with class absent (likely under development).

--- atlas-aba/trunk/src/main/java/org/incf/atlas/aba/resource/ABAServiceDAOImpl.java
+++ atlas-aba/trunk/src/main/java/org/incf/atlas/aba/resource/ABAServiceDAOImpl.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.incf.atlas.aba.util.ABAConfigurator;
-import org.incf.atlas.common.util.BaseDAO;
+//import org.incf.atlas.common.util.BaseDAO;
 
 /**
  * @author Asif Memon

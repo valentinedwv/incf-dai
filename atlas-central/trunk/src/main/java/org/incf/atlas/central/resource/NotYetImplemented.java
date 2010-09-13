7da2d8790737e6f273ea598bacfbf677d22d1a06
davlit0917 <davlit0917@7cbae6f4-d32f-11de-a711-9fae7a67fc1c>
2010-09-13 03:44:46 +0000

    Initial commit.

---
+++ atlas-central/trunk/src/main/java/org/incf/atlas/central/resource/NotYetImplemented.java
@@ -0,0 +1,46 @@
+package org.incf.atlas.central.resource;
+
+import org.incf.atlas.common.util.ExceptionCode;
+import org.incf.atlas.common.util.ExceptionHandler;
+import org.restlet.Context;
+import org.restlet.data.Request;
+import org.restlet.data.Response;
+import org.restlet.resource.Representation;
+import org.restlet.resource.ResourceException;
+import org.restlet.resource.Variant;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * This resource is used for functions planned but not yet implemented.
+ * 
+ * @author dave
+ */
+public class NotYetImplemented extends BaseResouce {
+	
+	private final Logger logger = LoggerFactory.getLogger(getClass());
+
+	String message;
+
+	public NotYetImplemented(Context context, Request request, 
+			Response response) {
+		super(context, request, response);
+		
+		logger.debug("Instantiated {}.", getClass());
+		
+		message = "This process has not yet been implemented.";
+		
+		logger.info(message);
+	}
+
+	@Override
+	public Representation represent(Variant variant) throws ResourceException {
+        
+        // prepare an ExceptionReport
+        ExceptionHandler exHandler = getExceptionHandler();
+        exHandler.addExceptionToReport(ExceptionCode.NOT_APPLICABLE_CODE, null, 
+                new String[] { message });
+        return getExceptionRepresentation();
+	}
+
+}
---
+++ atlas-central/trunk/src/main/java/org/incf/atlas/central/server/ServerApplication.java
@@ -0,0 +1,101 @@
+package org.incf.atlas.central.server;
+
+import org.incf.atlas.central.resource.Capabilities;
+import org.incf.atlas.central.resource.NotSupported;
+import org.incf.atlas.central.resource.NotYetImplemented;
+import org.incf.atlas.central.resource.PingResource;
+import org.incf.atlas.central.resource.ProcessDescriptions;
+import org.incf.atlas.central.resource.UnrecognizedUri;
+import org.incf.atlas.common.server.RequestQueryStrings;
+import org.restlet.Application;
+import org.restlet.Component;
+import org.restlet.Restlet;
+import org.restlet.Router;
+import org.restlet.data.Protocol;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.bridge.SLF4JBridgeHandler;
+
+public class ServerApplication extends Application 
+		implements RequestQueryStrings {
+
+	private static final Logger logger = LoggerFactory.getLogger(
+			ServerApplication.class);
+	
+	public ServerApplication() {
+
+		// redirect restlet logging to slf4j
+		// see http://wiki.restlet.org/docs_1.1/13-restlet/48-restlet/101-restlet.html
+		// "Replacing default JDK logging with log4j"
+		SLF4JBridgeHandler.install();
+
+		StringBuilder buf = new StringBuilder();
+		buf.append("\n");
+		buf.append("       *******************************************\n");
+		buf.append("       *          Starting INCF Server           *\n");
+		buf.append("       *******************************************\n");
+		buf.append("\n  Server version   : ");
+		buf.append("\nWaiting for connections ...");
+		logger.info(buf.toString());
+	}
+	
+	@Override
+	public Restlet createRoot() {
+		Router router = new Router(getContext());
+		
+		router.attach(GET_CAPABILITIES,            Capabilities.class);
+		router.attach(DESCRIBE_PROCESS,            ProcessDescriptions.class);
+		router.attach(GET_PROCESSES_BY_IDENTIFIER, NotYetImplemented.class);
+		router.attach(LIST_HUBS,                   NotYetImplemented.class);
+		router.attach(LIST_HUB_SERVICES,           NotYetImplemented.class);
+		router.attach(LIST_PROCESSES,              NotYetImplemented.class);
+		router.attach(DESCRIBE_SRS,                NotSupported.class);
+        router.attach(DESCRIBE_TRANSFORMATION,     NotSupported.class);
+        router.attach(GET_2D_IMAGES_BY_POI,        NotYetImplemented.class);
+        router.attach(GET_2D_IMAGES_BY_URI,        NotSupported.class);
+        router.attach(GET_CELLS_BY_POI,            NotSupported.class);
+        router.attach(GET_CELLS_BY_URI,            NotSupported.class);
+        router.attach(GET_CORRELATION_MAP_BY_POI,  NotSupported.class);
+        router.attach(GET_GENES_BY_POI,            NotSupported.class);
+        router.attach(GET_STRUCTURE_NAMES_BY_POI,  NotSupported.class);
+        router.attach(GET_TRANSFORMATION_CHAIN,    NotYetImplemented.class);
+		router.attach(LIST_SRS_S,                  NotYetImplemented.class);
+		router.attach(LIST_TRANSFORMATIONS,        NotYetImplemented.class);
+		router.attach(RETRIEVE_2D_IMAGE,           NotSupported.class);
+        router.attach(TRANSFORM_POI,               NotSupported.class);
+		
+		router.attach("/ping/{pingType}", PingResource.class);
+		
+		router.attachDefault(UnrecognizedUri.class);
+
+		return router;
+	}
+
+	// testing only
+	public static void main(String[] args) {
+
+		// redirect restlet logging to slf4j
+		// see http://wiki.restlet.org/docs_1.1/13-restlet/48-restlet/101-restlet.html
+		// "Replacing default JDK logging with log4j"
+		SLF4JBridgeHandler.install();
+
+		StringBuilder buf = new StringBuilder();
+		buf.append("\n");
+		buf.append("       *******************************************\n");
+		buf.append("       *          Starting INCF Server           *\n");
+		buf.append("       *******************************************\n");
+		buf.append("\n  Server version   : ");
+		buf.append("\nWaiting for connections ...");
+		logger.info(buf.toString());
+		
+		try {
+			Component component = new Component();
+			component.getServers().add(Protocol.HTTP, 8182);
+			component.getDefaultHost().attach(new ServerApplication());
+			component.start();
+		} catch (Exception e) {
+			logger.error("Exception in main().", e);
+		}
+	}
+
+}

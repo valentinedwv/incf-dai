f1aafb44a83be79ecd9987b8f20eebbd7989d14b
davlit0917 <davlit0917@7cbae6f4-d32f-11de-a711-9fae7a67fc1c>
2010-11-12 04:59:24 +0000

    Xml namespace context class.

--- atlas-central/trunk/src/main/java/org/incf/atlas/central/xml/XmlNameSpaceContext.java
+++ atlas-central/trunk/src/main/java/org/incf/atlas/central/xml/XmlNameSpaceContext.java
@@ -3,56 +3,163 @@ package org.incf.atlas.central.xml;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+import javax.xml.XMLConstants;
 import javax.xml.namespace.NamespaceContext;
 
 public class XmlNameSpaceContext implements NamespaceContext {
 
-	private HashMap<String, String> prefixUriMap = new HashMap<String, String>();
-	private HashMap<String, HashSet<String>> uriPrefixMap = new HashMap<String, HashSet<String>>();
+	// prefix --> uri (1:1)
+	private Map<String, String> prefixUriMap = new HashMap<String, String>();
+
+	// uri --prefix(es) (1:many)
+	private HashMap<String, Set<String>> uriPrefixesMap = 
+			new HashMap<String, Set<String>>();
+	
+	public XmlNameSpaceContext() {
+		
+		// prefix is "xml"
+		addNamespace(XMLConstants.XML_NS_PREFIX, XMLConstants.XML_NS_URI);
+		
+		// prefix is "xmlns"
+		addNamespace(XMLConstants.XMLNS_ATTRIBUTE, XMLConstants.XMLNS_ATTRIBUTE_NS_URI);
+		
+		// this is default, override with setDefaultNamespaceURI()
+		addNamespace(XMLConstants.DEFAULT_NS_PREFIX, XMLConstants.NULL_NS_URI);
+	}
+	
+	public void setDefaultNamespaceURI(String defaultNamespaceURI) {
+		addNamespace(XMLConstants.DEFAULT_NS_PREFIX, defaultNamespaceURI);
+	}
 
 	public void addNamespace(String prefix, String uri) {
-		this.prefixUriMap.put(prefix, uri);
-		if (this.uriPrefixMap.containsKey(uri)) {
-			this.uriPrefixMap.get(uri).add(prefix);
+		
+		// prefix --> uri
+		prefixUriMap.put(prefix, uri);
+		
+		// uri --> prefix(es)
+		if (uriPrefixesMap.containsKey(uri)) {
+			uriPrefixesMap.get(uri).add(prefix);
 		} else {
-			HashSet<String> set = new HashSet<String>();
-			set.add(prefix);
-			this.uriPrefixMap.put(uri, set);
+			Set<String> prefixes = new HashSet<String>();
+			prefixes.add(prefix);
+			uriPrefixesMap.put(uri, prefixes);
 		}
 	}
 
 	public String getNamespaceURI(String prefix) {
-		return this.prefixUriMap.get(prefix);
+		
+		// argument is null
+		if (prefix == null) {
+			throw new IllegalArgumentException();
+		}
+		
+		// prefix is bound
+		String namespaceUri = prefixUriMap.get(prefix);
+		if (namespaceUri != null) {
+			return namespaceUri;
+		}
+		
+		// prefix is unbound
+		return XMLConstants.NULL_NS_URI;
 	}
 
-	public String getPrefix(String namespaceURI) {
-		HashSet<String> set = this.uriPrefixMap.get(namespaceURI);
-		Iterator<String> i = set.iterator();
-		String result = null;
-		if (i.hasNext()) {
-			result = i.next();
+	public String getPrefix(String namespaceUri) {
+		
+		// argument is null
+		if (namespaceUri == null) {
+			throw new IllegalArgumentException();
 		}
-		return result;
+		
+		// uri is bound
+		Set<String> prefixes = uriPrefixesMap.get(namespaceUri);
+		if (prefixes != null) {
+			return prefixes.iterator().next();		// single or arbitrary
+		}
+	
+		// uri is unbound
+		return null;
 	}
 
 	public Iterator<String> getPrefixes(String namespaceURI) {
-		HashSet<String> set = this.uriPrefixMap.get(namespaceURI);
-		return set.iterator();
+		Set<String> prefixes = uriPrefixesMap.get(namespaceURI);
+		
+		// TODO this iterator should not support remove()
+		return prefixes.iterator();
 	}
+	
+	// not sure how to provide an iterator with disabled remove()
+//	public class namespacePrefixIterator<String> implements Iterator<String> {
+//		public void remove() {
+//			throw new UnsupportedOperationException();
+//		}
+//	}
 
+	/**
+	 * Removes the prefix and the namespace URI. If multiple prefixes map to
+	 * a single URI, remove only this prefix and not the URI.
+	 * 
+	 * @param prefix namespace prefix to be removed
+	 */
 	public void removePrefix(String prefix) {
-		this.prefixUriMap.remove(prefix);
+		if (prefix == null) {
+			return;
+		}
+		
+		// get uri for prefix, exit if none
+		String namespaceUri = prefixUriMap.get(prefix);
+		if (namespaceUri == null) {
+			return;
+		}
+		
+		// get all prefixes for uri
+		Set<String> prefixes = uriPrefixesMap.get(namespaceUri);
+		
+		// if not more than 1, remove from both maps
+		if (prefixes.size() <= 1) {
+			uriPrefixesMap.remove(namespaceUri);
+			prefixUriMap.remove(prefix);
+			return;
+		}
+		
+		// if other prefixes for uri, remove only this one
+		Iterator<String> it = prefixes.iterator();
+		while (it.hasNext()) {
+			String thisPrefix = it.next();
+			if (thisPrefix.equals(prefix)) {
+				it.remove();
+			}
+		}
+		prefixUriMap.remove(prefix);
 	}
 
-	public void removeUri(String uri) {
-		if (this.uriPrefixMap.containsKey(uri)) {
-			HashSet<String> set = this.uriPrefixMap.get(uri);
-			Iterator<String> i = set.iterator();
-			while (i.hasNext()) {
-				this.prefixUriMap.remove(i.next());
-			}
-			this.uriPrefixMap.remove(uri);
+	/**
+	 * Finds all prefixes for given URI, removes every prefix, then finally
+	 * removes the URI.
+	 * 
+	 * @param namespaceUri URI to be removed
+	 */
+	public void removeUri(String namespaceUri) {
+		if (namespaceUri == null) {
+			return;
 		}
+		
+		// get prefixes for uri, exit if none
+		Set<String> prefixes = uriPrefixesMap.get(namespaceUri);
+		if (prefixes == null) {
+			return;
+		}
+		
+		// for each prefix, remove prefix
+		Iterator<String> it = prefixes.iterator();
+		while (it.hasNext()) {
+			prefixUriMap.remove(it.next());
+		}
+		
+		// finally remover uri
+		uriPrefixesMap.remove(namespaceUri);
 	}
 	
 }

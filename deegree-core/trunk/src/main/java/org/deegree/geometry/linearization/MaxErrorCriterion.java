bff21ebd0d778540f35d12d3727676a23084ed39
davlit0917 <davlit0917@7cbae6f4-d32f-11de-a711-9fae7a67fc1c>
2011-01-26 18:56:42 +0000

    Initial partial commit.

---
+++ deegree-core/trunk/src/main/java/org/deegree/geometry/linearization/MaxErrorCriterion.java
@@ -0,0 +1,64 @@
+//$HeadURL: http://svn.wald.intevation.org/svn/deegree/deegree3/branches/3.0/deegree-core/src/main/java/org/deegree/geometry/linearization/MaxErrorCriterion.java $
+/*----------------------------------------------------------------------------
+ This file is part of deegree, http://deegree.org/
+ Copyright (C) 2001-2009 by:
+ Department of Geography, University of Bonn
+ and
+ lat/lon GmbH
+
+ This library is free software; you can redistribute it and/or modify it under
+ the terms of the GNU Lesser General Public License as published by the Free
+ Software Foundation; either version 2.1 of the License, or (at your option)
+ any later version.
+ This library is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ details.
+ You should have received a copy of the GNU Lesser General Public License
+ along with this library; if not, write to the Free Software Foundation, Inc.,
+ 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+ Contact information:
+
+ lat/lon GmbH
+ Aennchenstr. 19, 53177 Bonn
+ Germany
+ http://lat-lon.de/
+
+ Department of Geography, University of Bonn
+ Prof. Dr. Klaus Greve
+ Postfach 1147, 53001 Bonn
+ Germany
+ http://www.geographie.uni-bonn.de/deegree/
+
+ e-mail: info@deegree.org
+ ----------------------------------------------------------------------------*/
+package org.deegree.geometry.linearization;
+
+/**
+ * TODO add documentation here
+ * 
+ * @author <a href="mailto:schneider@lat-lon.de">Markus Schneider </a>
+ * @author last edited by: $Author: aionita $
+ * 
+ * @version $Revision: 20867 $, $Date: 2009-11-18 06:02:50 -0800 (Wed, 18 Nov 2009) $
+ */
+public class MaxErrorCriterion implements LinearizationCriterion {
+
+    private double maxError;
+
+    private int maxNumPoints;
+
+    public MaxErrorCriterion( double maxError, int maxNumPoints ) {
+        this.maxError = maxError;
+        this.maxNumPoints = maxNumPoints;
+    }
+
+    public double getMaxError() {
+        return maxError;
+    }
+
+    public int getMaxNumPoints() {
+        return maxNumPoints;
+    }
+}

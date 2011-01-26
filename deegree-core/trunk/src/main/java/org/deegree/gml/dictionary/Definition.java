898790ad68d7a70c91a602329a74fc786e893880
davlit0917 <davlit0917@7cbae6f4-d32f-11de-a711-9fae7a67fc1c>
2011-01-26 18:58:44 +0000

    Initial partial commit.

---
+++ deegree-core/trunk/src/main/java/org/deegree/gml/dictionary/Definition.java
@@ -0,0 +1,67 @@
+//$HeadURL: http://svn.wald.intevation.org/svn/deegree/deegree3/branches/3.0/deegree-core/src/main/java/org/deegree/gml/dictionary/Definition.java $
+/*----------------------------------------------------------------------------
+ This file is part of deegree, http://deegree.org/
+ Copyright (C) 2001-2009 by:
+ - Department of Geography, University of Bonn -
+ and
+ - lat/lon GmbH -
+
+ This library is free software; you can redistribute it and/or modify it under
+ the terms of the GNU Lesser General Public License as published by the Free
+ Software Foundation; either version 2.1 of the License, or (at your option)
+ any later version.
+ This library is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ details.
+ You should have received a copy of the GNU Lesser General Public License
+ along with this library; if not, write to the Free Software Foundation, Inc.,
+ 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+ Contact information:
+
+ lat/lon GmbH
+ Aennchenstr. 19, 53177 Bonn
+ Germany
+ http://lat-lon.de/
+
+ Department of Geography, University of Bonn
+ Prof. Dr. Klaus Greve
+ Postfach 1147, 53001 Bonn
+ Germany
+ http://www.geographie.uni-bonn.de/deegree/
+
+ e-mail: info@deegree.org
+ ----------------------------------------------------------------------------*/
+package org.deegree.gml.dictionary;
+
+import org.deegree.commons.tom.ows.CodeType;
+import org.deegree.commons.tom.ows.StringOrRef;
+import org.deegree.gml.GMLObject;
+
+/**
+ * Base interface for all GML definition objects.
+ * 
+ * @see Dictionary
+ * 
+ * @author <a href="mailto:schneider@lat-lon.de">Markus Schneider</a>
+ * @author last edited by: $Author: mschneider $
+ * 
+ * @version $Revision: 23152 $, $Date: 2010-03-18 03:20:56 -0700 (Thu, 18 Mar 2010) $
+ */
+public interface Definition extends GMLObject {
+
+    /**
+     * Returns the description.
+     * 
+     * @return the description, can be <code>null</code>
+     */
+    public StringOrRef getDescription();
+
+    /**
+     * Returns the names.
+     * 
+     * @return the names, can be empty, but never <code>null</code>
+     */
+    public CodeType[] getNames();
+}

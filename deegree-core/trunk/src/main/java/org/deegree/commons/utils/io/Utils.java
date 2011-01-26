77a9b4d6a785007f5d60a87632cd53c6f3f6bc20
davlit0917 <davlit0917@7cbae6f4-d32f-11de-a711-9fae7a67fc1c>
2011-01-26 18:50:24 +0000

    Initial partial commit.

---
+++ deegree-core/trunk/src/main/java/org/deegree/commons/utils/io/Utils.java
@@ -0,0 +1,61 @@
+//$HeadURL: http://svn.wald.intevation.org/svn/deegree/deegree3/branches/3.0/deegree-core/src/main/java/org/deegree/commons/utils/io/Utils.java $
+/*----------------------------------------------------------------------------
+ This file is part of deegree, http://deegree.org/
+ Copyright (C) 2001-2010 by:
+ - Department of Geography, University of Bonn -
+ and
+ - lat/lon GmbH -
+
+ This library is free software; you can redistribute it and/or modify it under
+ the terms of the GNU Lesser General Public License as published by the Free
+ Software Foundation; either version 2.1 of the License, or (at your option)
+ any later version.
+ This library is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ details.
+ You should have received a copy of the GNU Lesser General Public License
+ along with this library; if not, write to the Free Software Foundation, Inc.,
+ 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+ Contact information:
+
+ lat/lon GmbH
+ Aennchenstr. 19, 53177 Bonn
+ Germany
+ http://lat-lon.de/
+
+ Department of Geography, University of Bonn
+ Prof. Dr. Klaus Greve
+ Postfach 1147, 53001 Bonn
+ Germany
+ http://www.geographie.uni-bonn.de/deegree/
+
+ e-mail: info@deegree.org
+ ----------------------------------------------------------------------------*/
+package org.deegree.commons.utils.io;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * 
+ * @author <a href="mailto:schmitz@lat-lon.de">Andreas Schmitz</a>
+ * @author last edited by: $Author: aschmitz $
+ * 
+ * @version $Revision: 25906 $, $Date: 2010-08-16 06:21:53 -0700 (Mon, 16 Aug 2010) $
+ */
+public class Utils {
+
+    /**
+     * Output stream that loses all data.
+     */
+    public static final OutputStream DEV_NULL = new OutputStream() {
+        @Override
+        public void write( int b )
+                                throws IOException {
+            // lost
+        }
+    };
+
+}

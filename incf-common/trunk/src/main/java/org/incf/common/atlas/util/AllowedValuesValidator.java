f1a87796038c1867ca88a92d8fc0a35b1b130f74
davlit0917 <davlit0917@7cbae6f4-d32f-11de-a711-9fae7a67fc1c>
2011-01-31 22:00:27 +0000

    Change to pom and replacement data inputs validator.

--- incf-common/trunk/src/main/java/org/incf/common/atlas/util/AllowedValuesValidator.java
+++ incf-common/trunk/src/main/java/org/incf/common/atlas/util/AllowedValuesValidator.java
@@ -4,6 +4,10 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
+import org.deegree.services.controller.ows.OWSException;
+import org.deegree.services.wps.ProcessletInputs;
+import org.deegree.services.wps.input.LiteralInput;
+
 public class AllowedValuesValidator {
 	
 	private static final String PREFIX = XPathNamespaceResolver.DEFAULT_PREFIX;
@@ -137,22 +141,125 @@ Case 4: Optional Input, Any Value is Accepted, With a Default Value
 			return false;
 		}
 	}
-
-	public ValidationResult validateNEW(String dataInputKey, String dataInputValue) 
+	
+	private List<String> getAllowedValues(String dataInputKey) 
 			throws IOException {
-		ValidationResult validationResult = new ValidationResult();
+		String xPathExpression = FIRST + dataInputKey + ALLOWED_VALUES;
 
 		// get AllowedVales, size 0 implies no AllowedValues
-		String xPathExpression = FIRST + dataInputKey + ALLOWED_VALUES;
 		List<String> allowedValues = evaluator.evaluateXPath(xPathExpression);
+		if (allowedValues.size() == 0) {
+			return null;
+		}
+
+		return allowedValues;
+	}
+
+	private String getDefaultValue(String dataInputKey) 
+			throws IOException {
+		String xPathExpression = FIRST + dataInputKey + DEFAULT_VALUE;
 
 		// get DefaultValue, null implies no DefaultValue
-		xPathExpression = FIRST + dataInputKey + DEFAULT_VALUE;
 		List<String> defaultValues = evaluator.evaluateXPath(xPathExpression);
 		String defaultValue = null;
 		if (defaultValues.size() > 0) {
 			defaultValue = defaultValues.get(0);
 		}
+		return defaultValue;
+	}
+	
+	public String getValidatedStringValue(ProcessletInputs in, 
+            String dataInputKey) throws IOException, OWSException {
+		
+		// determine which data input case is setup for this data input
+		ValidationResult validationResult = new ValidationResult();
+		List<String> allowedValues = getAllowedValues(dataInputKey);
+		String defaultValue = getDefaultValue(dataInputKey);
+        int dataInputCase = determineCase(allowedValues, defaultValue);
+		
+        // see what request's data input looks like 
+        Object literalInput = in.getParameter(dataInputKey);
+        
+        // case A: literalInput is null: data input omitted
+        //  case 1, 2 - handled by deegree (i.e. we're not here)
+        //  case 3, 4 - dataInputValue = defaultValue
+        if (literalInput == null) {
+        	return defaultValue;		// case 3 or 4
+        }
+        
+        // get value of input
+        String dataInputValue = ((LiteralInput) literalInput).getValue();
+        
+        // case B: dataInputValue == null --> empty string x=;
+        //  case 1 - throw exception (a value is required)
+        //  case 2 - throw exception (value not allowed)
+        //  case 3, 4 - dataInputValue = defaultValue
+        if (dataInputValue == null) {
+        	switch (dataInputCase) {
+        	case 1:
+        		throw new OWSException("A value was not provided.", 
+        				OWSException.INVALID_PARAMETER_VALUE, dataInputKey);
+        	case 2:
+        		throw new OWSException("The value is not one of the allowed "
+        				+ "values. Check DescribeProcess.", 
+        				OWSException.INVALID_PARAMETER_VALUE, dataInputKey);
+        	}
+        	return defaultValue;		// case 3 or 4
+        }
+        
+        // case C: dataInputValue has value
+        //  case 1, 3 - accept value
+        //  case 2, 4 - validate value against allowed values
+        //    accept | throw exception (value not allowed)
+    	switch (dataInputCase) {
+    	case 1:
+    	case 3:
+    		return dataInputValue;
+    	}
+        
+		// case 2 or 4 -- validate input value against AllowedValues
+		for (String allowedValue : allowedValues) {
+			if (dataInputValue.equals(allowedValue)) {
+				return dataInputValue;
+			}
+		}
+		
+		// input value not allowed
+		throw new OWSException("Value '" + dataInputValue 
+				+ "' is not one of the allowed values. Check DescribeProcess.", 
+				OWSException.INVALID_PARAMETER_VALUE, dataInputKey);
+	}
+	
+	private int determineCase(List<String> allowedValues, String defaultValue) {
+		int dataInputCase = 0;
+		if (defaultValue == null) {
+			if (allowedValues == null) {
+				dataInputCase = 1;
+			} else {
+				dataInputCase = 2;
+			}
+		} else {
+			if (allowedValues == null) {
+				dataInputCase = 3;
+			} else {
+				dataInputCase = 4;
+			}
+		}
+		return dataInputCase;
+	}
+
+	public ValidationResult validateNEW(String dataInputKey, String dataInputValue) 
+			throws IOException {
+		ValidationResult validationResult = new ValidationResult();
+		
+		List<String> allowedValues = getAllowedValues(dataInputKey);
+		String defaultValue = getDefaultValue(dataInputKey);
+		
+		
+		
+		
+		
+
 		
 		// debug
 		//System.out.println("\ndataInputKey      : " + dataInputKey);

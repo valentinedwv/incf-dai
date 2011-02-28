1a22efe21854b4faffdc61796b565f66e58a161f
davlit0917 <davlit0917@7cbae6f4-d32f-11de-a711-9fae7a67fc1c>
2011-02-28 04:41:22 +0000

    [maven-release-plugin]  copy for tag ucsd-0.1.0

---
+++ incf-ucsd/tags/ucsd-0.1.0/src/main/java/org/incf/ucsd/atlas/util/BaseDAO.java
@@ -0,0 +1,222 @@
+/*
+ * @(#)BaseDAO.java, May 01, 2006, 12:44 PM
+ *
+ */
+package org.incf.ucsd.atlas.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+
+/**
+ * BaseDAO is an abstract class that provides connection methods
+ *
+ * @author	Asif Memon
+ *
+ * @version	4
+ * @history	4
+ * Date         Who   Description
+ * -----------  ----  ---------------------------------------------
+ * 23-JUN-2006  AM    Initial Version.
+ */
+public class BaseDAO {
+
+
+//--------------------------------Data Members----------------------------------
+
+
+    DataSource dataSource = null;
+
+
+//---------------------------Protected Methods----------------------------------
+
+
+    /** For Datasource(Connection Pooling)
+     * Gets the connection to the database.
+     *
+     * @param jndiName - The jndi name of the datasource
+     * @return A Connection object
+     * @throws DataAccessObjectException - If there is an exception establishing the connection.
+     *
+     */
+/*    protected Connection getConnection( String jndiName )
+            throws Exception {
+
+        boolean standAlone = true;
+
+        Connection connection = null;
+
+        BIRNConfigurator props = BIRNConfigurator.INSTANCE;
+
+            //This is done for connection pooling 
+            try {
+                connection = getDataSource( jndiName ).getConnection();
+            } catch ( SQLException sqle ) {
+                throw new DAOException( sqle.getMessage() );
+            } catch ( Exception e ) {
+                throw new DAOException( e.getMessage() );
+            }
+
+        return connection;
+
+    }  // end of getConnection method
+    
+*/
+
+
+    /**
+     * Gets the connection to the database. Does not do connection pooling.
+     *
+     * @param jndiName - The jndi name of the datasource
+     * @return A Connection object
+     * @throws DataAccessObjectException - If there is an exception establishing the connection.
+     *
+     */
+    public Connection getStandAloneConnection()// please change public to protected
+            throws Exception {
+
+    	System.out.println( "Start - getStandAloneConnection()");
+        
+    	Connection connection = null;
+
+        UCSDConfigurator props = UCSDConfigurator.INSTANCE;
+        
+        String driverClassName = props.getValue("server.database.driverClassName"); 
+        String dbUrl = props.getValue("server.database.atlasdburl"); 
+        String dbUser = props.getValue("server.database.atlasdbuser");
+        String dbPassword = props.getValue("server.database.atlasdbpassword");
+
+    	System.out.println( "driverClassName - "+driverClassName);
+    	System.out.println( "dbUrl - "+dbUrl);
+    	System.out.println( "dbUser - "+dbUser);
+    	System.out.println( "dbPassword - "+dbPassword);
+
+        try {
+        	Class.forName( driverClassName );
+        	System.out.println( "Start - getStandAloneConnection()");
+            connection = 
+            	DriverManager.getConnection( dbUrl, dbUser, dbPassword );
+            System.out.println( "End - getStandAloneConnection()");
+ 
+        } catch ( Exception e ) {
+        	e.printStackTrace();
+        	System.out.println( "There is an error" + e.getClass() + e.getMessage());
+            throw new Exception( e.getMessage() );
+        }
+
+        System.out.println( "End - getStandAloneConnection()");
+        return connection;
+
+    }// end of getStandAloneConnection method
+
+
+    /**
+     * Gets the connection to the database. Does not do connection pooling.
+     *
+     * @param jndiName - The jndi name of the datasource
+     * @return A Connection object
+     * @throws DataAccessObjectException - If there is an exception establishing the connection.
+     *
+     */
+    public Connection getStandAloneConnectionForPostgres()// please change public to protected
+            throws Exception {
+
+    	System.out.println( "Start - getStandAloneConnectionForPostgres()");
+        
+    	Connection connection = null;
+
+        UCSDConfigurator props = UCSDConfigurator.INSTANCE;
+        
+        String driverClassName = props.getValue("postgres.server.database.driverClassName"); 
+        String dbUrl = props.getValue("postgres.server.database.atlasdburl");
+        String dbUser = props.getValue("postgres.server.database.atlasdbuser");
+        String dbPassword = props.getValue("postgres.server.database.atlasdbpassword");
+
+        System.out.println( "driverClassName - "+driverClassName);
+        System.out.println( "dbUrl - "+dbUrl);
+        System.out.println( "dbUser - "+dbUser);
+        System.out.println( "dbPassword - "+dbPassword);
+ 
+        try {
+        	Class.forName( driverClassName );
+        	System.out.println( "Start - getStandAloneConnectionForPostgres()");
+            connection = 
+            	DriverManager.getConnection( dbUrl, dbUser, dbPassword );
+            System.out.println( "End - getStandAloneConnectionForPostgres()");
+ 
+        } catch ( Exception e ) {
+        	System.out.println( "There is an error" + e.getClass() + e.getMessage());
+            throw new Exception( e.getMessage() );
+        }
+
+        System.out.println( "End - getStandAloneConnectionForPostgres()");
+        return connection;
+
+    }// end of getStandAloneConnection method
+
+
+    /**
+     * Closes an open connection.
+     *
+     * @param connection - A connection object
+     */
+    protected void closeConnection( Connection connection ) {
+
+        try {
+            if ( connection != null || !connection.isClosed() ) {
+                connection.close();
+            }
+        } catch (Exception doNothing) {
+        } finally {
+            connection = null;
+        }
+    }
+
+
+//------------------------------- Private Methods -----------------------------
+
+
+    /**
+     * Returns data source to be used to get the connection object.
+     *
+     * @param jndiName - The jndi name for the data source
+     * @return Returns the DataSource object
+     * @throws Exception Ignores the exception and passes it to the caller
+     */
+    public DataSource getDataSource( String jndiName )
+            throws Exception {
+
+        // Throw the exception back to the caller
+        if ( dataSource == null ) {
+            Context context = new InitialContext();
+            dataSource = ( DataSource )context.lookup( jndiName );
+        }
+
+        return dataSource;
+
+    } // end of getDataSource
+
+    /**
+     * Returns data source to be used to get the connection object.
+     *
+     * @param jndiName - The jndi name for the data source
+     * @return Returns the DataSource object
+     * @throws Exception Ignores the exception and passes it to the caller
+     */
+    public void setDataSource( String jndiName )
+            throws Exception {
+
+        // Throw the exception back to the caller
+        if ( dataSource == null ) {
+            Context context = new InitialContext();
+            dataSource = ( DataSource )context.lookup( jndiName );
+        }
+
+    } // end of getDataSource
+
+
+}//end of DataAccessObject.java
---
+++ incf-ucsd/tags/ucsd-0.1.0/src/main/java/org/incf/ucsd/atlas/util/Paxinos2WHS.java
@@ -0,0 +1,77 @@
+package org.incf.ucsd.atlas.util;
+
+
+public class Paxinos2WHS {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Paxinos2WHS client = new Paxinos2WHS();
+		// client.getTransformation( 1.00, 4.3, 1.78 );
+		client.getTransformation( 0.0, 0.8, -3.88 );
+
+	}
+
+
+	public String getTransformation (double x, double y, double z) {
+
+		//flipped x in the powerpoint defined by Ilya
+		x = x * -1;
+		
+		String w = "";
+		double phi = 0;
+		double a = 0;
+		double b = 0;
+		double c = 0;
+		double theta = 0;
+
+		try {
+
+			a = 564 + 49*z;
+			b = 200 + 9*z + 0.5*z*z;
+			c = 250;
+
+			if ( z < -4.8 && z > -6.72) {
+				a += -30*(z+6.72) + 8*(z+6.72)*(z+6.72)*(z+6.72);
+			}
+
+			if ( z < -6.0 ) {
+				b -= 16;
+			} else if ( z < -5.6 ) {
+				b += (z+5.6)*40;
+			}
+
+			if ( z > 1.1 ) {
+				c = 250 + 6.15*(z-1.1);
+			}
+
+			theta = 0.046 + 0.0047*z;
+			
+			if ( z > -6 ) {
+				phi = -0.087;
+			}
+			if ( z > -1.5 && z < 1.7 ) {
+				phi = -0.174;
+			}
+			
+			String y1 = String.valueOf(Math.round(a - x * 54 * theta + (y-3) * 54 * phi));
+			String z1 = String.valueOf(512 - Math.round(b + (y-3) * 54));
+			String x1 = String.valueOf(Math.round(c + x * 54));
+			w = String.valueOf(x)+" "+String.valueOf(y)+" "+String.valueOf(z)+" "+x1+" "+y1+" "+z1;
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+
+		}
+
+		System.out.println("Transformed - " + w);
+		return w;
+
+	}
+
+	
+}
+

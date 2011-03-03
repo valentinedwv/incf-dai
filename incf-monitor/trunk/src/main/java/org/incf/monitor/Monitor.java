9d6065aee3f0a71263610d022caa1c48f7f28a68
davlit0917 <davlit0917@7cbae6f4-d32f-11de-a711-9fae7a67fc1c>
2011-03-03 19:13:31 +0000

    Updated for Deegree implementations.

--- incf-monitor/trunk/src/main/java/org/incf/monitor/Monitor.java
+++ incf-monitor/trunk/src/main/java/org/incf/monitor/Monitor.java
@@ -3,10 +3,12 @@ package org.incf.monitor;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.PrintStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
@@ -24,14 +26,23 @@ public class Monitor {
 	private Logger logger = LoggerFactory.getLogger(getClass());
 	
 	private static final String CONFIG_FILE = "/config.xml";
-	private static final String NEWLINE = "\n";
+	private static final int SERVER_LENGTH = "incf-dev-local".length();
+	private static final int PATH_LENGTH = "/central/atlas".length();
+	private static final int HUB_LENGTH = "central".length();
+	private static final int PROCESS_LENGTH = "GetTransformationChain".length();
 	
 	private DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
 	private DateFormat tf = new SimpleDateFormat("HH:mm:ss");
 	
 	private XMLConfiguration config;
-	private StringBuilder buf;
+	private String server;
+	private String currentHub;
 	private boolean anyFailure;
+	
+	// set up list for any failures
+	List<Failure> failures = new ArrayList<Failure>();
+	
+	private final PrintStream out = System.out;
 	
 	public Monitor() {
 		config = new XMLConfiguration();
@@ -42,7 +53,6 @@ public class Monitor {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		buf = new StringBuilder();
 	}
 	
 	/**
@@ -53,34 +63,33 @@ public class Monitor {
 		
 		logger.info("Starting monitor run.");
 		
-		// compose report header
+		// report header
 		Date now = new Date();
-		String header0 = "INCF Monitor: date: " + df.format(now) 
-			+ ", start: " + tf.format(now) + ", finish ";
-		String rule = "-------------------------------------------------------"
-			+ "------------------------------------------------";
-		String header1 = "   Time   HTTP Pass/";
-		String header2 = "   msec   Code Fail  Path           Query";
-		buf.append(rule).append(NEWLINE);
-		buf.append(header1).append(NEWLINE);
-		buf.append(header2).append(NEWLINE);
-		buf.append(rule).append(NEWLINE);
+		String horizLine = "-------------------------------------------------------"
+				+ "------------------------------------------------%n";
+		out.printf("INCF Monitor: date: %s, start: %s%n", df.format(now), tf.format(now));
+		out.printf(horizLine);
+		out.printf("   Time   HTTP Pass/%n");
+		out.printf("   msec   Code Fail  %s %s %s Data Inputs%n", 
+				padRight("Server", SERVER_LENGTH), 
+				padRight("Hub", HUB_LENGTH), 
+				padRight("Process", PROCESS_LENGTH));
+		out.printf(horizLine);
+
 
 		// read config file that itemizes tests
-		List<String> hostList = config.getList("hosts.host");
+		List<String> serverList = config.getList("servers.server");
 		
 		// read config file that itemizes tests
 		List<HierarchicalConfiguration> testList = config.configurationsAt(
 				"tests.test");
 		
-		// iterate set of hosts and run tests for each one
-		for(Iterator<String> hosts = hostList.iterator(); 
-				hosts.hasNext(); ) {
-		    String host = hosts.next();
-		    
-		    System.out.println("host: " + host);
-		    
-			buf.append(String.format("%s%n", host));
+		// iterate set of servers and run tests for each one
+		for(Iterator<String> servers = serverList.iterator(); 
+				servers.hasNext(); ) {
+		    server = servers.next();
+
+		    out.printf("%n%s%n", server);
 
 			// iterate set of tests and run each one
 			for(Iterator<HierarchicalConfiguration> test = testList.iterator(); 
@@ -89,16 +98,27 @@ public class Monitor {
 			    String uri = node.getString("uri");
 			    String responsePattern = node.getString("responsePattern");
 			    
-			    String resolvedUri = uri.replace("@HOST@", host);
+			    String resolvedUri = uri.replace("@SERVER@", server);
 			    
 			    runTest(resolvedUri, responsePattern);
 			}
 		}
 		
-		// compose report footer
-		header0 += tf.format(new Date()) + NEWLINE;
-		buf.insert(0, header0);
-		buf.append(rule);
+		// report footer
+		out.printf(horizLine);
+		if (failures.size() > 0) {
+			out.printf("Failures (server, path, process):%n");
+			for (Failure failure : failures) {
+				out.printf("  %s %s %s%n", 
+						padRight(failure.server.split("\\.")[0], SERVER_LENGTH),
+						padRight(failure.path.split("/")[1], HUB_LENGTH),
+						padRight(failure.process, PROCESS_LENGTH));
+			}
+		} else {
+			out.printf("No failures%n");
+		}
+		out.printf(horizLine);
+		out.printf("Finish: %s%n", tf.format(new Date()));
 		
 		// report if any failures
 		if (anyFailure) {
@@ -106,7 +126,9 @@ public class Monitor {
 				List<String> tosList = config.getList("reporting.email.tos.to");
 				String[] tos = tosList.toArray(new String[0]);
 				try {
-					sendEmail(tos, "INCF Hubs Status - Failure", buf.toString());
+					
+					// TODO send what
+					sendEmail(tos, "INCF Hubs Status - Failure", "");
 				} catch (EmailException e) {
 					logger.error("Email exception", e);
 				}
@@ -114,7 +136,7 @@ public class Monitor {
 		}
 		
 		if (config.getBoolean("debug")) {
-			System.out.println(buf.toString());
+//			System.out.println(buf.toString());
 		}
 		
 		logger.info("Finishing monitor run.");
@@ -128,6 +150,10 @@ public class Monitor {
 	 * @param responsePattern
 	 */
 	private void runTest(String uri, String responsePattern) {
+		if (uri.equals("Not applicable") || uri.equals("Planned")) {
+			return;
+		}
+		
 		URL url = null;
 		String s = null;
 		HttpURLConnection conn = null;
@@ -146,7 +172,9 @@ public class Monitor {
 			logger.error("Exception: ", e);
 		}
 		
-		conn.disconnect();
+		if (conn != null) {
+			conn.disconnect();
+		}
 		
 		// scan response and determine pass/fail
 		String result = null;
@@ -161,13 +189,49 @@ public class Monitor {
 		double elapsedTime = (double) (System.currentTimeMillis() - startTime) 
 				/ 1000;
 
+		// TODO extract Identifier value unless GC or DP
+		String process = null;
+		String dataInputs = "";
+		String query = url.getQuery();
+		String[] kvPairs = query.split("&");
+		if (kvPairs[1].contains("GetCapabilities")) {
+			process = "GetCapabilities";
+		} else if (kvPairs[2].contains("DescribeProcess")) {
+			process = "DescribeProcess";
+			int firstEqualsSymbol = kvPairs[3].indexOf('=');
+			dataInputs =  kvPairs[3].substring(firstEqualsSymbol + 1);
+		} else if (kvPairs[2].contains("Execute")) {
+			process = kvPairs[3].split("=")[1];
+			if (kvPairs.length > 4) {
+				int firstEqualsSymbol = kvPairs[4].indexOf('=');
+				dataInputs =  kvPairs[4].substring(firstEqualsSymbol + 1);
+			}
+		}
+		
+		//
+		String path = url.getPath();
+		String hub = path.split("/")[1];
+		
+		// blank line for new hub
+		if (!hub.equals(currentHub)) {
+			out.println();
+			currentHub = hub;
+		}
+		
 		// add line for this test to report
-		buf.append(String.format("%8.3f  %d  %s  %-14s %s%n",
-				elapsedTime, statusCode, result, leftJustify(url.getPath(), 14), 
-				url.getQuery()));
+		out.print(String.format("%8.3f  %d  %s  %s %s %s %s%n",
+				elapsedTime, statusCode, result, 
+				padRight(server.split("\\.")[0], SERVER_LENGTH),
+				padRight(hub, HUB_LENGTH), 
+				padRight(process, PROCESS_LENGTH), dataInputs));
+		
+		// accrue any failure
+		if (result.equals("FAIL")) {
+			failures.add(new Failure(server, path, process));
+		}
 	}
 	
-	private String leftJustify(String string, int width) {
+	private String padRight(String string, int width) {
 		int length = string.length();
 		if (length >= width) {
 			return string.substring(0, width);
@@ -205,6 +269,17 @@ public class Monitor {
     	}
     	reader.close();
     	return fileData.toString();
+    }
+    
+    private class Failure {
+    	private String server;
+    	private String path;
+    	private String process;
+    	private Failure(String server, String path, String process) {
+    		this.server = server;
+    		this.path = path;
+    		this.process = process;
+    	}
     }
 
 	public static void main(String[] args) {
